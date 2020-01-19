9c97387a4e7ece81879d2eb65fd257e855423ac1
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2020-01-19 19:15:37 +0100

    add image background to the app

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, ImageBackground } from 'react-native';
 import SearchInput from "./components/SearchInput"
+import getImageForWeather from "./utils/getImageForWeather"
 
 export default class App extends Component {
   render() {
@@ -9,16 +10,24 @@ export default class App extends Component {
        style={styles.container}
        behavior="padding"
       >
-        <Text style={[styles.textStyle, styles.largeText]}>
-          San Francisco
-        </Text>
-        <Text style={[styles.textStyle, styles.smallText]}>
-          Light Cloud
-        </Text>
-        <Text style={[styles.textStyle, styles.largeText]}>
-          24°
-        </Text>
-        <SearchInput placeholder="Search any city" />
+        <ImageBackground
+          source={getImageForWeather("Clear")}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        > 
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.textStyle, styles.largeText]}>
+              San Francisco
+            </Text>
+            <Text style={[styles.textStyle, styles.smallText]}>
+              Light Cloud
+            </Text>
+            <Text style={[styles.textStyle, styles.largeText]}>
+              24°
+            </Text>
+            <SearchInput placeholder="Search any city" />
+          </View>
+        </ImageBackground>
       </KeyboardAvoidingView>
     );
   }
@@ -28,8 +37,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  detailsContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1, 
+  },
+  imageStyle: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
   },
   textStyle: {
     textAlign: "center",
